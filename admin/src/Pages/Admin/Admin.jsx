741cfd573af30c4bef13e2bfb4cc5466290b4ce9
Alilymae <ortizaliah88@gmail.com>
2025-05-26 00:32:59 +0400

    Your commit message

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Admin.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import { Routes, Route } from 'react-router-dom'
-
+//COMPONENTS for admin panel
 import Addproducts from '../../Components/Addproducts/Addproducts'
 import Listproducts from '../../Components/Listproducts/Listproducts'
 
